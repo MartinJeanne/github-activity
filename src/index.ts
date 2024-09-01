437070dd57d964cca3ecb495c137686e82d9a9a1
MartinJeanne <martinjeanne.dev@gmail.com>
2024-09-01 11:24:50 +0200

    testing GitHub API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 console.log('hello world');
+const userUrl = 'https://api.github.com/users/MartinJeanne/events'
+
+async function fetchGitHubActivity() {
+    const data = await fetch(userUrl)
+        .then(json => json.json())
+        .catch(console.error);
+    console.log(data);
+}
+
+fetchGitHubActivity();
